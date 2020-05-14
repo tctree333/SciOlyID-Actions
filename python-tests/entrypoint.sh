da4c6e6b3c2e669bee5aa73d5daf615f06d49f67
Tomi Chen <tctree333@users.noreply.github.com>
2020-05-14 00:30:11 -0700

    update stuff

---
+++ python-tests/entrypoint.sh
@@ -0,0 +1,11 @@
+pip install -r requirements.txt
+pip install coverage pytest
+
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+chmod +x ./cc-test-reporter
+./cc-test-reporter before-build
+
+coverage run --source=bot/ -m pytest -x
+
+coverage xml
+./cc-test-reporter after-build --exit-code 0
