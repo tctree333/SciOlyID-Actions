5daa1e17573b8b40ebf33b53c277d03757565dd3
Tomi Chen <tctree333@users.noreply.github.com>
2020-05-15 00:10:42 -0700

    verbose?

--- update-redis/entrypoint.sh
+++ update-redis/entrypoint.sh
@@ -33,4 +33,4 @@ machine git.heroku.com
 export HEROKU_REDIS_URL="$(heroku redis:credentials REDIS_URL -a $INPUT_HEROKU_APP_NAME)"
 
 echo "### Updating REDIS_URL ###"
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$HOME/.ssh/server_key" dokku@$INPUT_DOKKU_HOST -- config:set -no-restart $INPUT_DOKKU_APP_NAME REDIS_URL="$HEROKU_REDIS_URL"
+ssh -vvv -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$HOME/.ssh/server_key" dokku@$INPUT_DOKKU_HOST -- config:set -no-restart $INPUT_DOKKU_APP_NAME REDIS_URL="$HEROKU_REDIS_URL"
