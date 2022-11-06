80fafe77b37321d8e5ac8253bc70918b3b24001f
Tomi Chen <tctree333@users.noreply.github.com>
2022-11-06 11:07:25 -0800

    try updating version

--- sentry/entrypoint.sh
+++ sentry/entrypoint.sh
@@ -7,11 +7,11 @@ export SENTRY_ORG="$INPUT_SENTRY_ORG"
 VERSION=$(sentry-cli releases propose-version)
 
 if [[ -z "${INPUT_SENTRY_PROJECT_2}" ]]; then
-    sentry-cli releases new -p $INPUT_SENTRY_PROJECT_1 $VERSION
+    sentry-cli releases new -p $INPUT_SENTRY_PROJECT_1 "$VERSION"
 else
-    sentry-cli releases new -p $INPUT_SENTRY_PROJECT_1 -p $INPUT_SENTRY_PROJECT_2 $VERSION
+    sentry-cli releases new -p $INPUT_SENTRY_PROJECT_1 -p $INPUT_SENTRY_PROJECT_2 "$VERSION"
 fi
 
-sentry-cli releases set-commits --auto $VERSION
-sentry-cli releases finalize $VERSION
-sentry-cli releases deploys $VERSION new -e "Deploy to Dokku"
+sentry-cli releases set-commits --auto "$VERSION"
+sentry-cli releases finalize "$VERSION"
+sentry-cli releases deploys "$VERSION" new -e "Deploy to Dokku"
